59f7e110abcc751484269a2d454b46799eb4bb71
Brian McDonald <brian.mcdonald@falmouth.ac.uk>
2020-11-14 14:28:31 +0000

    Setting up initial project

---
+++ Assets/Scripts/VolcanoControlScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VolcanoControlScript : MonoBehaviour
+{
+    [SerializeField]
+    ParticleSystem smokeUp;
+
+    [SerializeField]
+    float smokeUpMinEmission = 1.00f;
+    float smokeUpCurrentEmission = 0.0f;
+    [SerializeField]
+    float smokeUpMaxEmission = 10.0f;
+    [SerializeField]
+    float smokeUpEmissionSpeed = 10.0f;
+
+    private void Update()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            smokeUpCurrentEmission += Time.deltaTime* smokeUpEmissionSpeed;
+
+            smokeUpCurrentEmission = Mathf.Clamp(smokeUpCurrentEmission, smokeUpMinEmission, smokeUpMaxEmission);
+
+            var emission = smokeUp.emission;
+            emission.rateOverTime = smokeUpCurrentEmission;
+
+        }
+    }
+
+
+}
